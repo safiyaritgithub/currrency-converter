fd84b14fe1fa463a2575da5620a560e64136a145
safiyaritgithub <satyasafi79812@gmail.com>
2024-05-01 12:09:00 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { CurrencyConverter } from "./components/currencyConverter"
+export const App = () => {
+  return (
+    <div className='min-h-screen bg-gray-100 flex flex-col items-center justify-center'>
+     <div className="container">
+     <CurrencyConverter/>
+     </div>
+    </div>
+  )
+}
+
+
+
